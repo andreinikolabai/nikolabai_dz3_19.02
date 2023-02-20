5c8303506618b1c1cfdec8176efb90f8e714eea2
andreinikolabai <andreiplus2000@gmail.com>
2023-02-20 12:12:11 +0200

    concat

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let a = 'com';
-let b = 'put';
-let c = 'ers';
+let a = prompt('first row', '');
+let b = prompt('second row', '');
+let c = prompt('third row', '');
+alert(a.concat(b,c));
 
-console.log(a.concat(b,c));
+let e;
+e=Number(prompt("number",""));
 
-let e = 12846;
 let f = e.toString().split("").map(Number);
-
-console.log(f.join(" "));
+alert(f.join(" "));
