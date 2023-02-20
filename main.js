937034ea4fc70859432fb9614acffa401682af56
andreinikolabai <andreiplus2000@gmail.com>
2023-02-20 13:58:04 +0200

    finish

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ let b = prompt('second row', '');
 let c = prompt('third row', '');
 alert(a.concat(b,c));
 
-const d = prompt("number:").split('').map(Number).join(" ");
+const d = prompt("number:").split('').join(" ");
 alert(d);
